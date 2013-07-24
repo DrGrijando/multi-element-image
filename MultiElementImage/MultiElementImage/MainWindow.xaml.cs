45f221b74bb586dd08c0f4c220f3eb6a30c62853
DrGrijando <rubengp64@gmail.com>
2013-07-24 10:19:53 +0200

    Upload project

---
+++ MultiElementImage/MultiElementImage/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultiElementImage
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {        
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            image1.mainIcon.Source = new BitmapImage(new Uri("Images/laptop.png", UriKind.Relative));
+        }
+
+        private void RadioButton_Checked_2(object sender, RoutedEventArgs e)
+        {           
+            image1.mainIcon.Source = new BitmapImage(new Uri("Images/trashcan.png", UriKind.Relative));
+        }
+
+        private void RadioButton_Checked_3(object sender, RoutedEventArgs e)
+        {
+            image1.statusIcon.Source = new BitmapImage(new Uri("Images/warning.png", UriKind.Relative));
+        }
+
+        private void RadioButton_Checked_4(object sender, RoutedEventArgs e)
+        {
+            image1.statusIcon.Source = new BitmapImage(new Uri("Images/battery.png", UriKind.Relative));
+        }
+
+        private void RadioButton_Checked_5(object sender, RoutedEventArgs e)
+        {
+            image1.statusIcon.Source = new BitmapImage(new Uri("Images/null.png", UriKind.Relative));
+        }
+    }
+}
---
+++ MultiElementImage/MultiElementImage/MultiImage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultiElementImage
+{
+    /// <summary>
+    /// Lógica de interacción para UserControl1.xaml
+    /// </summary>
+    public partial class MultiImage : UserControl
+    {
+        public MultiImage()
+        {
+            InitializeComponent();
+
+            //this.mainIcon = new Image();
+            //this.mainIcon.Source = new BitmapImage(new Uri("Images/laptop.png", UriKind.Relative));
+            //this.statusIcon = new Image();
+            //this.statusIcon.Source = new BitmapImage(new Uri("Images/warning.png", UriKind.Relative));
+        }
+    }
+}
